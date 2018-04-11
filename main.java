33c95edd491d5cd6de8059ce182a07b2b6cacadd
HoellerJulian <j.hoeller05@outlook.com>
2018-04-11 13:55:28 +0200

    Test data read, Base loop made

--- main.java
+++ main.java
@@ -3,7 +3,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int betrag =Integer.parseInt(args[0]);
+		int anzahlMuenzen =Integer.parseInt(args[1]);
+		for (int n = 0; n <anzahlMuenzen; n++) {
+			int offset=2;
+			muenzen[n]= Integer.parseInt(args[offset+m]);
+		}
 	}
 
 }
