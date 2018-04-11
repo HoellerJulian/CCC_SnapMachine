5579aaeb7d2a5d45f2c453a2a002ef26faf362a1
HoellerJulian <j.hoeller05@outlook.com>
2018-04-11 14:14:05 +0200

    LV1 Complete

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		
 		int betrag =Integer.parseInt(args[0]);
 		int anzahlMuenzen =Integer.parseInt(args[1]);
 		int[] muenzen =new int[anzahlMuenzen];
@@ -12,11 +12,12 @@ public class main {
 		for (int n = 0; n <anzahlMuenzen; n++) {
 			int offset=2;
 			muenzen[n]= Integer.parseInt(args[offset+n]);
-			input=sc.nextInt();
-			betrag-=input;
-			System.out.println(betrag);
+			betrag-=muenzen[n];
+			
 		}
-		
+		if(betrag<0) {System.out.println("CHANGE "+betrag*(-1));}
+		else if(betrag==0) {}
+		else if(betrag>0) {System.out.println("MISSING "+betrag);}
 	}
 
 }
