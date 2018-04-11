6a7ceb3c0663ee5837f2cc75e1770df9e5ce7c44
HoellerJulian <j.hoeller05@outlook.com>
2018-04-11 14:47:27 +0200

    LV 2 Complete

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class main {
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -15,9 +17,26 @@ public class main {
 			betrag-=muenzen[n];
 			
 		}
-		if(betrag<0) {System.out.println("CHANGE "+betrag*(-1));}
+		if(betrag<0) {
+		System.out.print("CHANGE");
+		returncmd(betrag*(-1));
+		
+		}
 		else if(betrag==0) {}
 		else if(betrag>0) {System.out.println("MISSING "+betrag);}
+	}
+	
+	static protected void returncmd(int betrag) {
+		while(betrag>0) {
+		if(betrag-200>-1) {betrag-=200;System.out.print(" 200");}
+		else if(betrag-100>-1) {betrag-=100;System.out.print(" 100");}
+		else if(betrag-50>-1) {betrag-=50;System.out.print(" 50");}
+		else if(betrag-20>-1) {betrag-=20;System.out.print(" 20");}
+		else if(betrag-10>-1) {betrag-=10;System.out.print(" 10");}
+		else if(betrag-5>-1) {betrag-=5;System.out.print(" 5");}
+		else if(betrag-2>-1) {betrag-=2;System.out.print(" 2");}
+		else {betrag-=1;System.out.print(" 1");}
+		}
 	}
 
 }
